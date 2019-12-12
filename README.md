5ffb27a4289674760b43395649cbc8edbaacfc9c
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-12-12 15:37:55 -0500

    optimized solution

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Repo for keeping track of the algorithms I've practiced.
 
 romanToInt: https://leetcode.com/problems/roman-to-integer/
+twoSum: https://leetcode.com/problems/two-sum/
---
+++ twoSum.js
@@ -0,0 +1,40 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+Unclear from Leetcode if the nums array is ALWAYS going to be a sorted list
+*/
+
+// BRUTE FORCE
+
+let twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] + nums[i] === target) return [i, j];
+    }
+  }
+};
+
+// ASSUMING A SORTED LIST
+
+let twoSum = function(nums, target) {
+  let compliments = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i];
+
+    if (compliments.hasOwnProperty(curr)) return [compliments[curr], i];
+    else {
+      compliment = target - curr;
+      compliments[compliment] = i;
+    }
+  }
+};
